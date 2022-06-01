64210656c0e6d6c694875b7580c187812b85346a
jorge alexander vasquez <jorge.vasquezyepes@gmail.com>
2022-06-01 19:05:36 -0400

    cambio de version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        hola evaluacion2.
+        hola evaluacion2. jorge.
         </p>
         <a
           className="App-link"
